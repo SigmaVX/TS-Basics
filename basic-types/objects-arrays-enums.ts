344026ae062d7174c7f2ec8370fe34cb98445c61
SigmaVX <tonywible@gmail.com>
2022-11-20 12:12:55 -0500

    added object and tuple updates

--- basic-types/objects-arrays-enums.ts
+++ basic-types/objects-arrays-enums.ts
@@ -2,13 +2,26 @@ enum Role {
 	ADMIN = "Admin",
 	SUPPORT = 100,
 	AUTHOR = "5"
-}
+};
 
-const person = {
+const personTypeInference = {
 	name: "Tony",
 	age: 40,
-	hobbies: ["gaming", "reading"],
+	stuff: ["SCUBA", 10, true],
 	role: Role.ADMIN
 };
 
-console.log(person.role);
+const personExplicit : {
+	name: string,
+	age: number,
+	hobbies: string[],
+	role: [number, string]	
+} = {
+	name: "Jen",
+	age: 39,
+	hobbies: ["reading", "walking"],
+	role: [1, "Tester"]
+};
+
+console.log(personTypeInference.role);
+console.log(personExplicit.role);
